972501968aae38253b4873ee72e5ae8fc42e1f6b
diana1190 <dagutierrez1190@gmail.com>
2017-08-23 21:59:19 -0600

    Agregando el proyecto al github

---
+++ Lab1_DianaGutierrez_23538/Lab1_DianaGutierrez_23538/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_DianaGutierrez_23538
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + " Este es su primer programa");
+            Console.ReadLine();
+        }
+    }
+}
